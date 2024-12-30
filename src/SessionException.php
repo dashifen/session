5dca4257635b5b63cda009456bfaf87958a28ccf
David Dashifen Kees <dashifen@dashifen.com>
2024-12-30 14:17:35 -0500

    upgrading library to PHP 8 (#1)
    * starting upgrade
    
    * quote switch

--- src/SessionException.php
+++ src/SessionException.php
@@ -5,5 +5,5 @@ namespace Dashifen\Session;
 use Dashifen\Exception\Exception;
 
 class SessionException extends Exception {
-	public const CANNOT_START = 1;
+	public const int CANNOT_START = 1;
 }
--- src/SessionInterface.php
+++ src/SessionInterface.php
@@ -7,82 +7,85 @@ namespace Dashifen\Session;
  *
  * @package Dashifen\Session
  */
-interface SessionInterface {
-	public const defaultIndex = 'Dashifen\Session\Session::index';
-	
-	/**
-	 * Returns true if the session has recorded the logging in of an authenticated visitor
-	 *
-	 * @return bool
-	 */
-	public function isAuthenticated(): bool;
-	
-	/**
-	 * Records the logging in of an authenticated visitor.
-	 *
-	 * @param string $username
-	 * @param array  $parameters
-	 *
-	 * @return void
-	 */
-	public function login(string $username, array $parameters = []): void;
-	
-	/**
-	 * Removes information about a visitor's authenticated session
-	 *
-	 * @return void
-	 */
-	public function logout();
-	
-	/**
-	 * Destroys a session.
-	 *
-	 * @return void
-	 */
-	public function destroy(): void;
-	
-	/**
-	 * Effectively, returns $_SESSION[$index] or $default
-	 *
-	 * @param string $index
-	 * @param mixed  $default
-	 *
-	 * @return mixed
-	 */
-	public function get(string $index, $default = null);
-	
-	/**
-	 * Effectively, determines if $_SESSION[$index] exists
-	 *
-	 * @param string $index
-	 *
-	 * @return bool
-	 */
-	public function exists(string $index): bool;
-	
-	/**
-	 * Effectively, $_SESSION[$index] = $value
-	 *
-	 * @param string $index
-	 * @param mixed  $value
-	 *
-	 * @return void
-	 */
-	public function set(string $index, $value): void;
-	
-	/**
-	 * Effectively, unset($_SESSION[$index])
-	 *
-	 * @param $index
-	 *
-	 * @return void
-	 */
-	public function remove(string $index): void;
-	
-	/**
-	 * Returns the session information managed herein
-	 *
-	 * @return array
-	 */
-	public function getSession(): array;
+interface SessionInterface
+{
+  public const string DEFAULT_INDEX = 'Dashifen\Session\Session::index';
+  public const string AUTHENTICATED = 'Dashifen\Session\Session::authentic';
+  public const string USERNAME = 'Dashifen\Session\Session::username';
+  
+  /**
+   * Returns true if the user's current session is authenticated.
+   *
+   * @return bool
+   */
+  public function isAuthenticated(): bool;
+  
+  /**
+   * Records the logging in of an authenticated visitor.
+   *
+   * @param string $username
+   * @param array  $parameters
+   *
+   * @return SessionInterface
+   */
+  public function login(string $username, array $parameters = []): SessionInterface;
+  
+  /**
+   * Removes information about a visitor's authenticated session
+   *
+   * @return SessionInterface
+   */
+  public function logout(): SessionInterface;
+  
+  /**
+   * Fully destroys a session; not just our data, all data.
+   *
+   * @return void
+   */
+  public function destroy(): void;
+  
+  /**
+   * Returns the data we've stored at $index or $default.
+   *
+   * @param string $index
+   * @param mixed  $default
+   *
+   * @return mixed
+   */
+  public function get(string $index, mixed $default = null): mixed;
+  
+  /**
+   * Effectively, determines if $_SESSION[$index] exists
+   *
+   * @param string $index
+   *
+   * @return bool
+   */
+  public function exists(string $index): bool;
+  
+  /**
+   * Returns true if the specified index exists within our session.
+   *
+   * @param string $index
+   * @param mixed  $value
+   *
+   * @return SessionInterface
+   */
+  public function set(string $index, mixed $value): SessionInterface;
+  
+  /**
+   * Removes the value stored at $index.
+   *
+   * @param string $index
+   *
+   * @return SessionInterface
+   */
+  public function remove(string $index): SessionInterface;
+  
+  /**
+   * Returns the session information managed herein
+   *
+   * @return array
+   */
+  public function getSession(): array;
 }
